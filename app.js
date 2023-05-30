bcd4e7e9cb4608346e1d81e8e301b74be39fbb08
Fernando Ramos <fndo.ramos@gmail.com>
2023-05-30 19:33:41 +0200

    lab solved

--- app.js
+++ app.js
@@ -4,25 +4,18 @@ const app = express();
 
 app.use(express.static("public"));
 
-
 app.get("/", (req, res, next) => {
-    res.sendFile(__dirname + "/views/homepage.html");
+  res.sendFile(__dirname + "/views/homepage.html");
 });
 
 app.get("/about", (req, res, next) => {
-    res.sendFile(__dirname + "/views/about.html");
+  res.sendFile(__dirname + "/views/about.html");
 });
 
 app.get("/works", (req, res, next) => {
-    res.sendFile(__dirname + "/views/works.html");
+  res.sendFile(__dirname + "/views/works.html");
 });
 
-
-
-
-
-
-
 app.listen(3001, () => {
-    console.log("server is listening")
+  console.log("server is listening");
 });
