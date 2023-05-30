70c002904ec0301d61debd292144ec34265cd062
Fernando Ramos <fndo.ramos@gmail.com>
2023-05-30 16:51:12 +0200

    add nav bar with routes and h1 for homepage html

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+
+
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + "/views/homepage.html");
+});
+
+app.get("/about", (req, res, next) => {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (req, res, next) => {
+    res.sendFile(__dirname + "/views/works.html");
+});
 
 
 
